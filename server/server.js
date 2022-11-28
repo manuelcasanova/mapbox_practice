65c9cf45e1bd4ccc9562bccdab95773cd431462a
Manuel Casanova <manucasanova@hotmail.com>
2022-11-28 14:59:42 -0800

    Install pg package, connect pg and create a route to get all points

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 3500;
+const pool = require('./config/db')
 
 app.set("view engine", 'ejs');
 
@@ -20,6 +21,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //middleware for cookies
-app.use(cookieParser());
+app.use(cookieParser())
+
+app.get('/', (req, res) => {
+  res.json("Test")
+})
+
+app.get("/points", async (req, res) => {
+  try {
+    const points = await pool.query(
+    'SELECT * FROM points'
+    );
+    res.json(points.rows)
+  } catch (err) {
+    console.error(err.message)
+  }
+});
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
