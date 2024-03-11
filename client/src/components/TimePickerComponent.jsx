f0bada711ed028ea91a8deae28083909e1e904e5
Manuel Casanova <manucasanova@hotmail.com>
2024-03-11 08:39:16 -0700

    Uninstall react time picker and create a custome component for that purpose

--- client/src/components/TimePickerComponent.jsx
+++ client/src/components/TimePickerComponent.jsx
@@ -1,14 +1,56 @@
-import React from 'react';
-import TimePicker from 'react-time-picker';
+import React, { useState } from 'react';
 
-export default function TimePickerComponent({ time, setTime }) {
+const TimePickerComponent = ({ time, setTime }) => {
+  const [hours, setHours] = useState('12');
+  const [minutes, setMinutes] = useState('00');
+
+  const handleHoursChange = (event) => {
+    let newHours = parseInt(event.target.value);
+    if (newHours < 0 || newHours > 23 || isNaN(newHours)) {
+      newHours = ""; // Reset to 0 if input is invalid
+    }
+    setHours(newHours);
+    updateTime(newHours, minutes);
+  };
+
+  const handleMinutesChange = (event) => {
+    let newMinutes = parseInt(event.target.value);
+    if (newMinutes < 0 || newMinutes > 59 || isNaN(newMinutes)) {
+      newMinutes = ""; // Reset to 0 if input is invalid
+    }
+    setMinutes(newMinutes);
+    updateTime(hours, newMinutes);
+  };
+
+  const updateTime = (newHours, newMinutes) => {
+    const newTime = `${newHours < 10 ? '0' + newHours : newHours}:${newMinutes < 10 ? '0' + newMinutes : newMinutes}`;
+    setTime(newTime);
+  };
+
   return (
-    <div>
-      <TimePicker
-        onChange={setTime}
-        value={time}
-        className='react-time-picker'
-      />
+    <div className="time-picker">
+      <div>
+        {/* <label>Hours:</label> */}
+        <input 
+          type="number" 
+          min="0" 
+          max="23" 
+          value={hours} 
+          onChange={handleHoursChange} 
+        />
+      </div>
+      <div>
+        <label>:</label>
+        <input 
+          type="number" 
+          min="0" 
+          max="59" 
+          value={minutes} 
+          onChange={handleMinutesChange} 
+        />
+      </div>
     </div>
   );
-}
+};
+
+export default TimePickerComponent;
