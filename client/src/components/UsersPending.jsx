5a4801eef4a4ba9c5740a2126c6b53842b82cbd6
Manuel Casanova <manucasanova@hotmail.com>
2024-04-25 12:37:40 -0700

    Move isNewRequest to a util function

--- client/src/components/UsersPending.jsx
+++ client/src/components/UsersPending.jsx
@@ -7,6 +7,7 @@ import axios from 'axios';
 import fetchUsernameAndId from "./util_functions/FetchUsername";
 import fetchPendingUsers from "./util_functions/FetchPendingUsers";
 import fetchLoginHistory from "./util_functions/FetchLoginHistory";
+import fetchIsNewRequest from "./util_functions/follow_functions/FetchIsNewRequest";
 
 const PendingUsers = () => {
 
@@ -160,27 +161,7 @@ const PendingUsers = () => {
   // Sort the login history array by login time in descending order
 loginhistory.sort((a, b) => new Date(b.login_time) - new Date(a.login_time));
 
-let isNewRequest = false;
-
-// Check if there are at least two login entries
-if (loginhistory.length >= 2) {
-  // Extract the second-to-last login time
-  const secondToLastLoginTime = new Date(loginhistory[1].login_time);
-
-  // Compare the lastmodification timestamp with the second-to-last login time
-  isNewRequest = pendingUsersObject.some(user => user.lastmodification > secondToLastLoginTime);
-  
-} else if (loginhistory.length === 1) {
-  // If there is only one login entry
-  const onlyLoginTime = new Date(loginhistory[0].login_time);
-  isNewRequest = pendingUsersObject.some(user => user.lastmodification > onlyLoginTime);
-} else {
-  // If there are no login entries
-  console.log('User has no login entries.');
-}
-
-console.log('isNewRequest:', isNewRequest);
-
+const isNewRequest = fetchIsNewRequest(pendingUsersObject, loginhistory);
 
   if (isLoading) {
     return <div>Loading...</div>;
---
+++ client/src/components/util_functions/follow_functions/FetchIsNewRequest.jsx
@@ -0,0 +1,29 @@
+// util_functions/FetchIsNewRequest.js
+
+const fetchIsNewRequest = (pendingUsersObject, loginhistory) => {
+  // Sort the login history array by login time in descending order
+  loginhistory.sort((a, b) => new Date(b.login_time) - new Date(a.login_time));
+
+  let isNewRequest = false;
+
+  // Check if there are at least two login entries
+  if (loginhistory.length >= 2) {
+    // Extract the second-to-last login time
+    const secondToLastLoginTime = new Date(loginhistory[1].login_time);
+
+    // Compare the lastmodification timestamp with the second-to-last login time
+    isNewRequest = pendingUsersObject.some(user => user.lastmodification > secondToLastLoginTime);
+    
+  } else if (loginhistory.length === 1) {
+    // If there is only one login entry
+    const onlyLoginTime = new Date(loginhistory[0].login_time);
+    isNewRequest = pendingUsersObject.some(user => user.lastmodification > onlyLoginTime);
+  } else {
+    // If there are no login entries
+    console.log('User has no login entries.');
+  }
+
+  return isNewRequest;
+};
+
+export default fetchIsNewRequest;
