69abcac5b5835df4af4ffdf7cc13d68fabee0c62
Manuel Casanova <manuelcasanova@Manuels-MacBook-Air.local>
2024-05-02 16:31:52 -0700

    Install dependencies, add env

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,84 +1,84 @@
-// const bcrypt = require('bcrypt');
-// const pool = require('../config/db');
-// const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const pool = require('../config/db');
+const jwt = require('jsonwebtoken');
 
-// const handleLogin = async (req, res) => {
-//   const { pwd, trimmedEmail } = req.body;
-//   //console.log(req.body)
-//   if (!pwd || !trimmedEmail) return res.status(400).json({ 'message': 'Email and password are required.' });
+const handleLogin = async (req, res) => {
+  const { pwd, trimmedEmail } = req.body;
+  //console.log(req.body)
+  if (!pwd || !trimmedEmail) return res.status(400).json({ 'message': 'Email and password are required.' });
 
-//   //See if the email exists MONGODB
-//   //const foundEmail = await User.findOne({ email: email }).exec();
-//   // if (!foundEmail) return res.sendStatus(401); //401 Unauthorized
+  //See if the email exists MONGODB
+  //const foundEmail = await User.findOne({ email: email }).exec();
+  // if (!foundEmail) return res.sendStatus(401); //401 Unauthorized
 
-//   try {
-//     const data = await pool.query('SELECT * FROM users WHERE email = $1', [trimmedEmail])
-//     const foundEmail = data.rows;
-//     // console.log(foundEmail)
-//     if (foundEmail.length === 0) {
-//       res.status(400).json({
-//         error: "No user registered"
-//       })
-//     } else {
-//       bcrypt.compare(pwd, foundEmail[0].password, (err, result) => {
-//         if (err) {
-//           res.status(500).json({
-//             error: "Server error"
-//           });
-//         } else if (result === true) {
+  try {
+    const data = await pool.query('SELECT * FROM users WHERE email = $1', [trimmedEmail])
+    const foundEmail = data.rows;
+    // console.log(foundEmail)
+    if (foundEmail.length === 0) {
+      res.status(400).json({
+        error: "No user registered"
+      })
+    } else {
+      bcrypt.compare(pwd, foundEmail[0].password, (err, result) => {
+        if (err) {
+          res.status(500).json({
+            error: "Server error"
+          });
+        } else if (result === true) {
 
 
-//           //Grab the roles
-//           const userId = foundEmail[0]._id;
-//           const roles = Object.values(foundEmail[0].roles).filter(Boolean);
-//           //Create JWTs Token. To send to use with the other routes that we want protected in our API.
-//           const accessToken = jwt.sign(
-//             {
-//               "UserInfo": {
-//                 "email": foundEmail[0].email,
-//                 "roles": roles
-//               }
-//             },
-//             process.env.ACCESS_TOKEN_SECRET,
-//             { expiresIn: '20m' } //In production, a few minutes.
-//           );
-//           const refreshToken = jwt.sign(
-//             { "username": foundEmail[0].username },
-//             process.env.REFRESH_TOKEN_SECRET,
-//             { expiresIn: '1h' }
-//           );
-//           //Save refreshToken with current user
+          //Grab the roles
+          const userId = foundEmail[0]._id;
+          const roles = Object.values(foundEmail[0].roles).filter(Boolean);
+          //Create JWTs Token. To send to use with the other routes that we want protected in our API.
+          const accessToken = jwt.sign(
+            {
+              "UserInfo": {
+                "email": foundEmail[0].email,
+                "roles": roles
+              }
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '20m' } //In production, a few minutes.
+          );
+          const refreshToken = jwt.sign(
+            { "username": foundEmail[0].username },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1h' }
+          );
+          //Save refreshToken with current user
 
-//           foundEmail[0].refreshToken = refreshToken;
+          foundEmail[0].refreshToken = refreshToken;
           
-//           pool.query('UPDATE users SET refreshtoken=$1 WHERE email=$2', [refreshToken, trimmedEmail])
-//           // console.log(result) //Delete before production
+          pool.query('UPDATE users SET refreshtoken=$1 WHERE email=$2', [refreshToken, trimmedEmail])
+          // console.log(result) //Delete before production
 
-//           //http cookie not accesible by js (for security. More secure than localstorage or another cookie)
-//           //remove secure: true temporarily if want to test with tunder client. Back in for production
-//           res.cookie('jwt', refreshToken, {
-//             httpOnly: true, sameSite: "None",
-//             secure: true,
-//             maxAge: 24 * 60 * 60 * 1000
-//           })
-//           res.json({ userId, roles, accessToken });
-//           // res.json({'success': `user ${user} is logged in`});
-
-
-
-//         } else {
-//           if (result !== true) res.status(401).json({ error: "Enter correct password" });
-//         }
-//       })
-//     }
-
-
-//   } catch (error) {
-//     console.log(error)
-//   };
-
-// };
+          //http cookie not accesible by js (for security. More secure than localstorage or another cookie)
+          //remove secure: true temporarily if want to test with tunder client. Back in for production
+          res.cookie('jwt', refreshToken, {
+            httpOnly: true, sameSite: "None",
+            secure: true,
+            maxAge: 24 * 60 * 60 * 1000
+          })
+          res.json({ userId, roles, accessToken });
+          // res.json({'success': `user ${user} is logged in`});
 
 
 
-// module.exports = { handleLogin };
+        } else {
+          if (result !== true) res.status(401).json({ error: "Enter correct password" });
+        }
+      })
+    }
+
+
+  } catch (error) {
+    console.log(error)
+  };
+
+};
+
+
+
+module.exports = { handleLogin };
