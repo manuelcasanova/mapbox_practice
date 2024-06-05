c67d525c2924b3261137fa08679b66c7f848585a
Manuel Casanova <manuelcasanova@Manuels-MacBook-Air.local>
2024-06-05 15:39:00 -0700

    Filter buttons css

--- client/src/components/RidesFilter.jsx
+++ client/src/components/RidesFilter.jsx
@@ -101,7 +101,7 @@ const RideFilter = ({ rides, onFilter, handleShowFilter }) => {
   return (
     <div className='filter-container'>
          <button
-      className='filter-button-close hide-big'
+      className='red-button  hide-big'
       onClick={() => handleShowFilter()}>x</button>
       <div className='filter-range'>
         <label className='filter-label'>Dates:</label>
@@ -160,9 +160,9 @@ const RideFilter = ({ rides, onFilter, handleShowFilter }) => {
       </div>
       {/* <button onClick={handleFilter}>Apply Filters</button> */}
       <div className='filter-range'>
-      <button title="Clear filter"  className='filter-button' onClick={() => {clearFilter(); handleFilter();}}><FontAwesomeIcon icon={faUndo}></FontAwesomeIcon></button>
+      <button title="Clear filter"  className='orange-button' onClick={() => {clearFilter(); handleFilter();}}><FontAwesomeIcon icon={faUndo}></FontAwesomeIcon></button>
       <button
-      className='filter-button-close hide-small'
+      className='red-button  hide-small'
       onClick={() => handleShowFilter()}>x</button>
       </div>
     </div>
